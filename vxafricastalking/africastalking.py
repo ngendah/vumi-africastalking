068a9b9ceb96bf586201b693197dcbd3b1d8df74
Ngenda Henry <ngendahk@gmail.com>
2019-02-17 11:36:26 +0300

    Add incoming sms messages handler

--- vxafricastalking/africastalking.py
+++ vxafricastalking/africastalking.py
@@ -1,3 +1,4 @@
+import json
 from treq.client import HTTPClient
 
 from twisted.internet import reactor
@@ -26,11 +27,48 @@ class AfricasTalkingTransportConfig(HttpRpcTransportConfig):
     )
 
 
+class JsonDecoder:
+    @staticmethod
+    def decode(content_length, content):
+        return json.load(content) if content_length else dict()
+
+
+class TextDecoder:
+    @staticmethod
+    def decode(content_length, content):
+        return dict(content=content.read() if content_length else '')
+
+
+class ContentDecoder(object):
+    def __init__(self, request):
+        self.content = request.content
+        self.headers = request.requestHeaders
+        self.decoder = self._decoder(self.headers)
+
+    @staticmethod
+    def _decoder(headers):
+        content_decoders = {
+            'application/json': JsonDecoder,
+        }
+        content_type = headers.getRawHeaders('content-type')
+        c_type = content_type[0] if type(content_type) is list else content_type
+        if ';' in c_type:
+            c_type = c_type.split(';')[0]
+        return content_decoders.get(c_type, TextDecoder)
+
+    def values(self):
+        content_length = int(self.headers.getRawHeaders('content-length')[0])
+        return self.decoder.decode(content_length, self.content)
+
+
 class AfricasTalkingTransport(HttpRpcTransport):
     transport_type = 'sms'
     transport_name = 'at_transport'
 
     CONFIG_CLASS = AfricasTalkingTransportConfig
+
+    EXPECTED_FIELDS = {'date', 'to', 'from', 'text'}
+    OPTIONAL_FIELDS = {'id', 'linkId', 'networkCode'}
 
     @property
     def agent_factory(self):
@@ -89,6 +127,55 @@ class AfricasTalkingTransport(HttpRpcTransport):
         validate = yield self.validate_outbound(response)
         validate['message_id'] = message_id
         yield self.outbound_status(**validate)
+
+    @inlineCallbacks
+    def handle_raw_inbound_message(self, message_id, request):
+        values, errors = self.get_field_values(
+            request,
+            self.EXPECTED_FIELDS
+        )
+        if errors:
+            self.emit('Invalid message: %s' % (errors,))
+            yield self.finish_request(message_id, json.dumps(errors), code=400)
+            return
+        yield self.publish_message(
+            message_id=message_id,
+            date=values['date'],
+            content=values['text'],
+            to_addr=values['to'],
+            from_addr=values['from'],
+            transport_type=self.transport_type,
+            transport_metadata={
+                'id': values.get('id'),
+                'network_code': values.get('networkCode'),
+                'link_id': values.get('linkId'),
+            },
+        )
+        yield self.finish_request(
+            message_id,
+            json.dumps({'message_id': message_id})
+        )
+
+    def get_field_values(self, request, expected_fields, ignored_fields=frozenset()):
+        if request.method == 'GET':
+            return super(AfricasTalkingTransport, self).get_field_values(
+                request,
+                expected_fields,
+                ignored_fields
+            )
+        values = {}
+        errors = {}
+        content_values = ContentDecoder(request).values()
+        for field in content_values.keys():
+            if field not in (expected_fields | ignored_fields):
+                if self._validation_mode == self.STRICT_MODE:
+                    errors.setdefault('unexpected_parameter', []).append(field)
+            else:
+                values[field] = content_values.get(field)
+        for field in expected_fields:
+            if field not in values:
+                errors.setdefault('missing_parameter', []).append(field)
+        return values, errors
 
     @inlineCallbacks
     def validate_outbound(self, response):

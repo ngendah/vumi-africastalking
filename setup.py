38c67ca77ad69618c2723695a228d0e8a27880a9
Ngenda Henry <ngendahk@gmail.com>
2019-02-17 13:11:17 +0300

    Clean up and correctly setup the project enviroment

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
+import os
 from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    filepath = os.path.join(os.path.abspath(os.path.dirname(__file__)), filename)
+    return open(filepath, 'r').read()
+
 
 setup(
     name='vumi-africastalking',
-    version=open('VERSION', 'r').read().strip(),
+    version=read_file('VERSION'),
     license='BSD',
     description='AfricasTalking transport for Vumi and Junebug',
-    long_description=open('README.rst', 'r').read(),
+    long_description=read_file('README.rst'),
     author='Ngenda Henry',
     author_email='ngendahk@gmail.com',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'vumi>=0.6.0',
         'mock'
